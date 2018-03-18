42c9ac36ab86179d7974e195ef83130257775d6e
Charles Carriere <charles@revelry.co>
2018-03-18 12:43:51 -0500

    add another post and edit config file

---
+++ content/sample-posts/18-03-2018/hackathon-pt2.md
@@ -0,0 +1,4 @@
+Wow. Not much sleep. I think you forget how to sleep as you get older. I'm excited about today. Wearing my NASA shirt because I'm expecting to launch today!
+
+9:30am: grabbed an espresso and donuts from District Donuts. Side note: I LOVE that place.
+9:45am: arrived at Scandy to find that Evan had beaten me here. We discuss donuts, sleep (or lack thereof) and code. Cole arrives. Donut consumption and status updates ensue.
--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,10 @@ module.exports = {
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet',
-  {
-    resolve: `gatsby-source-contentful`,
-    options:  {
-      spaceId: contentfulConfig[process.env.NODE_ENV].spaceId,
-      accessToken: contentfulConfig[process.env.NODE_ENV].accessToken,
-      host: process.env.NODE_ENV === "development" ? `preview.contentful.com` : `contentful.com`,
-    },
-  },
-],
+    {
+      resolve: `gatsby-source-contentful`,
+      options: process.env.NODE_ENV === "development" ? contentfulConfig.development : contentfulConfig.production
+    }
+  ],
 };
+
